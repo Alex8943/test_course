e6668d0d2b1607aff2cfccd41d0d3d3cb06d682a
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-10-03 17:02:35 +0200

    first assignmeent done

---
+++ 01_Introduction_assignment_cal/calculater.test.js
@@ -0,0 +1,38 @@
+const {add, subtract, multiply, divide} = require('./calculater'); 
+
+describe('add', () => {
+    beforeAll(() => {
+        console.log('beforeAll');
+    }); 
+    it('adds two numbers', () => {
+        expect(add(1, 2)).toBe(3);
+    });
+}); 
+
+describe('subtract', () => {
+    beforeAll(() => {
+        console.log('beforeAll');
+    }); 
+    it('subtracts two numbers', () => {
+        expect(subtract(1, 2)).toBe(-1);
+    });
+});
+
+describe('multiply', () => {
+    beforeAll(() => {
+        console.log('beforeAll');
+    }); 
+    it('multiplies two numbers', () => {
+        expect(multiply(1, 2)).toBe(2);
+    });
+});
+
+describe('divide', () => {
+    beforeAll(() => {
+        console.log('beforeAll');
+    }); 
+    it('divides two numbers', () => {
+        expect(divide(10, 5)).toBe(2);
+    });
+});
+
