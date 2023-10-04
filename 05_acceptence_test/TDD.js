93c9960a6f92a941ab0cf601ea2022713b0935ce
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-10-04 10:22:26 +0200

    Test driven develupment assignment add

---
+++ 05_acceptence_test/TDD.js
@@ -0,0 +1,27 @@
+//TDD = test driven development
+
+
+const data_sctucture = function (word){
+    const result = {}; 
+
+    for (const letter of word){
+        result[letter] = 1;
+    }
+
+    return result;
+}
+
+const anagrams = function (word1, word2){
+
+    const cleanWord1 = word2.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const cleanWord2 = word2.replace(/[^a-zA-Z]/g, '').toLowerCase();
+
+    const sortedWord1 = cleanWord1.split('').sort().join('');
+    const sortedWord2 = cleanWord2.split('').sort().join('');
+
+    return sortedWord1 === sortedWord2;
+}
+
+exports.anagrams = anagrams;
+
+exports.data_sctucture = data_sctucture;
---
+++ 05_acceptence_test/TDD.test.js
@@ -0,0 +1,27 @@
+const {data_sctucture, anagrams} = require('./TDD');
+
+describe('data_sctucture', () => {
+    beforeAll(() => {
+        console.log('Test data_sctucture');
+    });
+
+    it('should return an object with the letters of the word as keys and the value 1 as value', () => {
+        expect(data_sctucture("hello")).toEqual({"h": 1, "e": 1, "l": 1, "o": 1});
+        expect(data_sctucture("hello")).not.toEqual({"h": 2, "e": 2, "l": 2, "o": 2});
+        expect(data_sctucture("hello")).not.toEqual({"h": 1, "e": 1, "l": 1, "o": 1, "a": 1});
+    });
+});
+
+describe('anagrams', () => {
+    beforeAll(() => {
+        console.log('Test anagrams');
+    });
+    it('should return true if the words are anagrams', () => {
+        expect(anagrams("hello", "olleh")).toEqual(true);
+        expect(anagrams("hello", "olleh")).not.toEqual(false);
+    });
+    it('should return false if the words are not anagrams', () => {
+        expect(anagrams("hello", "hello")).not.toEqual(false);
+        expect(anagrams("hello", "hello")).not.toEqual(false);
+    });
+}); 
